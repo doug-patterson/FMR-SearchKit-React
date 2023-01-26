2767a18fe6fbd662e4b41630d1a158f46b0e25cb
Doug Patterson <doug@heyporter.app>
2023-01-25 18:56:01 -0800

    missing import

--- src/PaginatorStatic.js
+++ src/PaginatorStatic.js
@@ -1,7 +1,6 @@
 // we need a paginator, probably based on <input type="radio" name="pageSize" />
 // that sets the value of pageSize to 0, the total, next, last or `undefined` depending
 // and then is styled up to look like the other Paginator
-
 const PaginatorStatic = () => 'Static Paginator'
 
 export default PaginatorStatic
--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import React from 'react'
 export { includeSubmittedSearch } from './util'
 import SearchController from './SearchController'
 import HTTPSearchHOC from './HTTPSearchHOC'
