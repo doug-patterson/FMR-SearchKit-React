c4c09112c560de21e1186d29f17ab253b3909a30
Timothy Hoover <64495114+timothyhoover@users.noreply.github.com>
2023-02-10 11:29:43 -0600

    fix boolean filter styling

--- src/BooleanFilter.js
+++ src/BooleanFilter.js
@@ -1,20 +1,22 @@
 import React from 'react'
 import _ from 'lodash/fp'
 
-const BooleanFilter = ({ checked, onChange, name, UIComponents }) => (
+const BooleanFilter = ({ checked, onChange, name, UIComponents, layout }) => (
   <UIComponents.CardBody>
-      <UIComponents.CheckBox
-        checked={checked}
-        label={'Yes'}
-        {...(onChange
-          ? {
-              onChange: val => onChange({ checked: val })
-            }
-          : {
-              name
-            })}
-      />
-    </UIComponents.CardBody>
+    <UIComponents.CheckBox
+      checked={checked}
+      label={'Yes'}
+      textRight={name}
+      layout={layout}
+      {...(onChange
+        ? {
+            onChange: val => onChange({ checked: val })
+          }
+        : {
+            name
+          })}
+    />
+  </UIComponents.CardBody>
 )
 
 export default BooleanFilter
