910585922f6e43cc9a0b0a0d3797c8aa61004d08
Timothy Hoover <64495114+timothyhoover@users.noreply.github.com>
2023-02-10 11:37:29 -0600

    use label for labels

--- src/BooleanFilter.js
+++ src/BooleanFilter.js
@@ -1,12 +1,19 @@
 import React from 'react'
 import _ from 'lodash/fp'
 
-const BooleanFilter = ({ checked, onChange, name, UIComponents, layout }) => (
+const BooleanFilter = ({
+  checked,
+  onChange,
+  name,
+  UIComponents,
+  layout,
+  label
+}) => (
   <UIComponents.CardBody>
     <UIComponents.CheckBox
       checked={checked}
       label={'Yes'}
-      textRight={name}
+      textRight={label}
       layout={layout}
       {...(onChange
         ? {
