da750fc87d4f5ffb5c8c006d0947c84d43eb13cb
Timothy Hoover <64495114+timothyhoover@users.noreply.github.com>
2023-03-29 16:21:34 -0400

    add prop types to default UI components

--- src/types.ts
+++ src/types.ts
@@ -1,4 +1,11 @@
-import { InputHTMLAttributes } from 'react'
+import {
+  ButtonHTMLAttributes,
+  CSSProperties,
+  InputHTMLAttributes,
+  OptionHTMLAttributes,
+  ReactNode,
+  SelectHTMLAttributes
+} from 'react'
 
 export interface CheckBoxProps extends InputHTMLAttributes<HTMLInputElement> {
   textMiddle?: string
@@ -9,4 +16,32 @@ export interface CheckBoxProps extends InputHTMLAttributes<HTMLInputElement> {
 
 export interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
   focus: boolean
+}
+
+export interface GridProps {
+  children: ReactNode
+  rows: CSSProperties['gridTemplateRows']
+  columns: CSSProperties['gridTemplateColumns']
+  areas: CSSProperties['gridTemplateAreas']
+  gap: CSSProperties['gap']
+  style: CSSProperties
+}
+
+export interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  children: ReactNode
+}
+
+export interface SelectProps extends SelectHTMLAttributes<HTMLSelectElement> {
+  options: OptionHTMLAttributes<HTMLOptionElement>[]
+}
+
+export interface NavProps {
+  children: ReactNode
+  direction: CSSProperties['flexDirection']
+}
+
+export interface NavItemProps {
+  label: string
+  onClick: () => void
+  icon: JSX.Element
 }
